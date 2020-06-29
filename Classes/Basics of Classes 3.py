56c1f1d1d1ce48f0436c78fb2e8e94366e3d7260
Harshit Mody <55501384+harshitmody72@users.noreply.github.com>
2020-06-29 21:11:44 +0530

    Add files via upload

---
+++ Classes/Basics of Classes 3.py
@@ -0,0 +1,11 @@
+class New:
+    def __init__(self, x,y):
+        self.x= x
+        self.new= y
+    def tr(self):
+        print(f'hii{self.new}')
+
+
+point = New(9,10)
+print(point.new)
+point.tr()
