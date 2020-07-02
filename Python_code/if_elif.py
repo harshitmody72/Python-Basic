f0d4aabdc1ce89ab10f2c5cdac3ed6f9a1aa3c43
Harshit Mody <55501384+harshitmody72@users.noreply.github.com>
2020-07-02 21:45:44 +0530

    Add files via upload

---
+++ Python_code/if_elif.py
@@ -0,0 +1,20 @@
+condition = True
+if condition :
+    print("True")
+
+
+Condition = False
+if Condition :
+    print("True")
+
+else :
+    print("False")
+
+is_condition = False
+is_con = True
+
+if is_condition :
+    print("True")
+
+elif is_con:
+    print("False")
