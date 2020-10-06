b4cd4da056947bfba0fac07cc6cc66aed21d26e1
harshit5995 <56270504+harshit5995@users.noreply.github.com>
2020-10-06 09:20:15 +0530

    Update ARROW_Pattern.py

--- Python_code/ARROW_Pattern.py
+++ Python_code/ARROW_Pattern.py
@@ -3,3 +3,12 @@ for i in range(1, 5):
 
 for i in range(4, 0, -1):
     print('*' * i)
+    
+for i in range(1,6):
+    print(" "*(i-1),end='')
+    
+    for j in range(1,i+1):
+        print(j,end=")
+    
+    print("")    
+  
